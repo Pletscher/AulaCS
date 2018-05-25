c992d664ce05860777d66225a1d5f6fce42a0064
Pletscher <jujupletscher@gmail.com>
2018-05-24 22:35:36 -0300

    Criando programa Interfaces

---
+++ Aula1/Aula1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carro c = new Carro();
+
+            Console.WriteLine("Carro criado,velocidade inicial: " + c.Velocidade);
+            c.Acelerar();
+            c.Acelerar();
+
+            AcelerarBatante(c);
+            Console.WriteLine("Carro, velocidade atual: " + c.Velocidade);
+
+            Console.ReadKey();
+
+        }
+
+
+        static void AcelerarBatante(IMeioTransporte meioTransporte)
+        {
+            for (int i=0; i<10; i++)
+            {
+                meioTransporte.Acelerar();
+            }
+        }
+
+    }
+}
